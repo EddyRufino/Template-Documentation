1980baf60bddd4164f7d7324a31cb35f39a8e96d
Eddy Rufino <eddyjaair@gmail.com>
2020-09-18 19:27:25 -0500

    Category[-mcf] - Content[-mcf] - SubContent[-mcf]

---
+++ database/factories/SubContentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SubContent;
+use Faker\Generator as Faker;
+
+$factory->define(SubContent::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
